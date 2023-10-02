d80ed8f853b9f18d3f24defe0ce7eda1d2b28c9d
Strangedip <sg02295@gmail.com>
2023-10-02 13:57:48 +0530

    basic login

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-
+from flask import request
 from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+# from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 
 # create and configure the app
@@ -10,8 +12,15 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
+
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    email = request.values.get('email')
+    password = request.values.get('password')
+    if email == 'admin@gmail.com' and password == 'admin':
+        return render_template('home.html', msg="true")
+
+    return redirect(url_for('login'))
+
 
 app.run(debug=True)
