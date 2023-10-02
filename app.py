dcaf447cdb4661cc920a36a36f16925d1bfb43c5
Strangedip <sg02295@gmail.com>
2023-10-03 04:43:50 +0530

    database and login added

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
-import os
 from flask import request
-from flask import Flask, render_template
-from flask import Flask, render_template, redirect, url_for
-# from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+from sqlalchemy.sql import func
 
 
 # create and configure the app
 app = Flask(__name__)
 
-@app.route('/')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/sg022/Desktop/Git/ProjectRD/database.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     firstname = db.Column(db.String(30), nullable=False)
+#     lastname = db.Column(db.String(30), nullable=False)
+#     email = db.Column(db.String(30), unique=True, nullable=False)
+#     password = db.Column(db.String(30), nullable=False)
+
+#     def __repr__(self):
+#         return f'<{self.firstname}>'
+
+
+def verify(email, password):
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    user = conn.execute('SELECT * FROM user WHERE email = ?', (email,)).fetchone()
+    conn.close()
+    if user is None:
+        return False
+    if password == user['password']:
+        return True
+    return False
+
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
@@ -17,10 +45,20 @@ def login():
 def home():
     email = request.values.get('email')
     password = request.values.get('password')
-    if email == 'admin@gmail.com' and password == 'admin':
+    if verify(email, password):
         return render_template('home.html', msg="true")
 
-    return redirect(url_for('login'))
+    return redirect(url_for('invalid_login_msg'))
+
+
+@app.route('/invalid-credentials')
+def invalid_login_msg():
+    return render_template('error.html')
+
+
+@app.route('/create')
+def create():
+    return render_template('error.html')
 
 
 app.run(debug=True)
