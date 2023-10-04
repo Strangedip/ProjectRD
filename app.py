b4a65fee62dc0584f688835f64944c7975c81fca
Strangedip <sg02295@gmail.com>
2023-10-04 23:50:52 +0530

    inventory

--- app.py
+++ app.py
@@ -8,26 +8,27 @@ from sqlalchemy.sql import func
 # create and configure the app
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/sg022/Desktop/Git/ProjectRD/database.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/sg022/Desktop/Git/ProjectRD/database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     firstname = db.Column(db.String(30), nullable=False)
-#     lastname = db.Column(db.String(30), nullable=False)
-#     email = db.Column(db.String(30), unique=True, nullable=False)
-#     password = db.Column(db.String(30), nullable=False)
+class Inventory(db.Model):
+    Item_ID = db.Column(db.Integer, primary_key=True)
+    Item_Name = db.Column(db.String(50), nullable=False)
+    Item_Price = db.Column(db.Integer, nullable=False)
+    Item_Count = db.Column(db.Integer, nullable=False)
 
-#     def __repr__(self):
-#         return f'<{self.firstname}>'
+    def __repr__(self):
+        return f'<{self.Item_Name}>'
+
 
 
 def verify(email, password):
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
-    user = conn.execute('SELECT * FROM user WHERE email = ?', (email,)).fetchone()
+    user = conn.execute(
+        'SELECT * FROM user WHERE email = ?', (email,)).fetchone()
     conn.close()
     if user is None:
         return False
@@ -54,6 +55,26 @@ def home():
 @app.route('/invalid-credentials')
 def invalid_login_msg():
     return render_template('error.html')
+
+
+@app.route('/inventory')
+def inventory():
+    return render_template('inventory.html')
+
+
+@app.route('/create-order')
+def create_order():
+    return render_template('create-order.html')
+
+
+@app.route('/current-orders')
+def current_orders():
+    return render_template('current-orders.html')
+
+
+@app.route('/order-history')
+def order_history():
+    return render_template('order-history.html')
 
 
 @app.route('/create')
